b2d8a080ddce37a13ecda348e1108cf253a66641
Vitalii_Velich <silens088@gmail.com>
2022-02-02 19:31:18 +0300

    added new configs and test

---
+++ src/test/java/autotests/config/demowebshop/App.java
@@ -0,0 +1,7 @@
+package autotests.config.demowebshop;
+
+import org.aeonbits.owner.ConfigFactory;
+
+public class App {
+    public static AppConfig config = ConfigFactory.create(AppConfig.class, System.getProperties());
+}
